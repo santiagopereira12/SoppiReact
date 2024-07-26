89f98735255460bd48e446f6c3064263752e0480
santiagopereira12 <spereira98@misena.edu.co>
2024-07-25 20:02:39 -0500

    Implementación de Iconos

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react'
+import { PlusIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../context'
 
 const Card = (data) => {
@@ -10,7 +11,7 @@ const Card = (data) => {
                 <img className="w-full h-full object-cover rounded-lg" src={data.data.category.image} alt={data.data.title}/>
                 <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
                 onClick={() => context.setCount(context.count + 1)}>
-                    +
+                    <PlusIcon className='h-6 w-6 text-black'></PlusIcon>
                 </div>
             </figure>
             <p className='flex justify-between'>
