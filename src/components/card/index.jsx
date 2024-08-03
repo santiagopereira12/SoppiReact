4eba32c1fe5567786bb92488701727c80d149d17
santiagopereira12 <spereira98@misena.edu.co>
2024-08-02 20:42:19 -0500

    Implementación de cursor-pointer

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -16,8 +16,8 @@ const Card = (data) => {
         onClick={() => showProduct(data.data)}>
             <figure className='relative mb-2 w-full h-4/5'>
                 <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.data.category.name}</span>
-                <img className='w-full h-full object-cover rounded-lg' src={data.data.images} alt={data.data.title}/>
-                <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
+                <img className='w-full h-full object-cover rounded-lg cursor-pointer' src={data.data.images} alt={data.data.title}/>
+                <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1 cursor-pointer'
                 onClick={() => context.setCount(context.count + 1)}>
                     <PlusIcon className='h-6 w-6 text-black'></PlusIcon>
                 </div>
