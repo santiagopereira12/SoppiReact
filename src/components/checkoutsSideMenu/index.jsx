258cc892ab0ca34375617c9e7632504bea64e82d
santiagopereira12 <spereira98@misena.edu.co>
2024-08-06 17:23:21 -0500

    Eliminar Productos del Carrito

--- src/components/checkoutsSideMenu/index.jsx
+++ src/components/checkoutsSideMenu/index.jsx
@@ -6,7 +6,11 @@ import './styles.css'
 
 const CheckoutSideMenu = () => {
     const context = useContext(ShoppingCartContext)
-    console.log('CART: ', context.cartProducts)
+
+    const handleDelete = (id) =>{
+        const filteredProducts = context.cartProducts.filter(product => product.id != id)
+        context.setCartProducts(filteredProducts)
+    }
 
     return (
         <aside className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
@@ -24,9 +28,11 @@ const CheckoutSideMenu = () => {
                     context.cartProducts.map(product => (
                         <OrderCard
                             key={product.id}
+                            id={product.id}
                             title={product.title}
                             imageUrl={product.images}
                             price={product.price}
+                            handleDelete={handleDelete}
                         />
                     ))
                 }
