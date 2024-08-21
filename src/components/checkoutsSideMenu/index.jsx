c2ddfa76b1520a638b613d6667f24a3043237fec
santiagopereira12 <spereira98@misena.edu.co>
2024-08-20 23:37:46 -0500

    Suma total de productos en el carrito

--- src/components/checkoutsSideMenu/index.jsx
+++ src/components/checkoutsSideMenu/index.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../context'
 import OrderCard from '../../components/orderCard'
+import { totalPrice } from '../../utils'
 import './styles.css'
 
 const CheckoutSideMenu = () => {
@@ -37,7 +38,12 @@ const CheckoutSideMenu = () => {
                     ))
                 }
             </div>
-
+            <div className='px-6'>
+                <p className='flex justify-between items-center'>
+                    <span className='font-light'>Total:</span>
+                    <span className='font-medium text-2xl'>${totalPrice(context.cartProducts)}</span>
+                </p>
+            </div>
         </aside>
     )
 }
