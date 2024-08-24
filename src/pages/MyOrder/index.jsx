0acbf48597b667b1c2648a0cbbf56b332c1201a3
santiagopereira12 <spereira98@misena.edu.co>
2024-08-23 23:14:47 -0500

    Checkout de productos en el carrito

--- src/pages/MyOrder/index.jsx
+++ src/pages/MyOrder/index.jsx
@@ -1,9 +1,27 @@
+import { useContext } from "react"
+import { ShoppingCartContext } from "../../context"
+import OrderCard from "../../components/orderCard"
 import Layout from "../../components/layout"
 
 function MyOrder(){
+    const context = useContext(ShoppingCartContext)
+
     return (
         <Layout>
             MyOrder
+            <div className='flex flex-col w-8'>
+                {
+                    context.order?.slice(-1)[0].products.map(product => (
+                        <OrderCard
+                            key={product.id}
+                            id={product.id}
+                            title={product.title}
+                            imageUrl={product.images}
+                            price={product.price}
+                        />
+                    ))
+                }
+            </div>
         </Layout>
     )
 }
